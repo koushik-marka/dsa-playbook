8426daf39e1308baff13c2cf927bd603348ebc65
koushik <koushik.luffy007@gmail.com>
2026-06-26 16:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (59.19%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean checkString(String s) {
-        if(s.lastIndexOf("a")<s.indexOf("b") || s.indexOf("b")==-1) return true;
-        return false;
+        return !(s.contains("ba"));
     }
 }
