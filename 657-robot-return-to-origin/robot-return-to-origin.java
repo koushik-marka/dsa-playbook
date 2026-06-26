9bb6284ff1911f9d89fa42fbf6bcf6f80cc9ffb2
koushik <koushik.luffy007@gmail.com>
2026-06-26 16:35:36 +0530

    Time: 4 ms (98.80%) | Memory: 46.2 MB (20.15%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean judgeCircle(String moves) {
         int x=0;
         int y=0;
-        for(int i=0;i<moves.length();i++){
-            if(moves.charAt(i)=='U') y++;
-            else if(moves.charAt(i)=='D') y--;
-            else if(moves.charAt(i)=='L') x--;
-            else if(moves.charAt(i)=='R') x++;
+        for(char ch : moves.toCharArray()){
+            if(ch=='U') y++;
+            else if(ch=='D') y--;
+            else if(ch=='L') x--;
+            else if(ch=='R') x++;
         }
         return x==0 && y==0;
     }
