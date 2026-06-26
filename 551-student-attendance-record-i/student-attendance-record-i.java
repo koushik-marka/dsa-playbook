67a19a025fdd2c51f438feb4f33b395a68969af5
koushik <koushik.luffy007@gmail.com>
2026-06-26 09:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (44.33%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean checkRecord(String s) {
-        if(s.contains("LLL")) return false;
-        int a=s.indexOf('A');
-        if(a!=s.length()-1){
-            if(s.substring(a+1).indexOf('A')!=-1) return false;
-        }
-        
-        
-
-        return true;
+        return !s.contains("LLL") && s.indexOf('A') == s.lastIndexOf('A');
     }
 }
