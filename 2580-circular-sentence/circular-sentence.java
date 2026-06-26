b28970105a162ab48fd6f3a3f23a63cd147b8f76
koushik <koushik.luffy007@gmail.com>
2026-06-26 16:22:07 +0530

    Time: 1 ms (96.49%) | Memory: 43.3 MB (53.07%) - LeetSync

--- 2580-circular-sentence/circular-sentence.java
+++ 2580-circular-sentence/circular-sentence.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isCircularSentence(String s) {
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==' '){
-                if(s.charAt(i-1)!=s.charAt(i+1)) return false;
-            }
-            
-            
+        String t=s;
+        int c=0;
+        while(s.indexOf(" ")!=-1){
+            c++;
+            int a=s.indexOf(" ");
+            if(s.charAt(a-1)==s.charAt(a+1)) s=s.substring(a+1);
+            else return false;
         }
 
-        if( s.charAt(0)!=s.charAt(s.length()-1)) return false;
+        if(c==0 && s.charAt(0)!=s.charAt(s.length()-1)) return false;
+        if(t.charAt(0)!=t.charAt(t.length()-1)) return false;
         return true;
     }
 }
