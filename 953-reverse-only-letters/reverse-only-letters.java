5a1808ce797a1289015a852725f1d9b470c5bc9c
koushik <koushik.luffy007@gmail.com>
2026-06-29 23:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (44.79%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char arr[]=s.toCharArray();
+        int i=0;
+        int j=arr.length-1;
+        while(i<j){
+            if(Character.isLetter(arr[i]) && Character.isLetter(arr[j])){
+                char temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j--;
+            }
+            else if(!Character.isLetter(arr[i])){
+                i++;
+            }
+            else if(!Character.isLetter(arr[j])){
+                j--;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return String.valueOf(arr);
+    }
+}
