bcc779777602bf2d52d42f05b63d457c31505997
koushik <koushik.luffy007@gmail.com>
2026-06-29 20:26:22 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (74.41%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -5,8 +5,8 @@ class Solution {
             fq[ch-'a']++;
         }
         int a=fq[s.charAt(0)-'a'];
-        for(int i=0;i<s.length();i++){
-            if(fq[s.charAt(i)-'a']!=a){
+        for(int i=0;i<26;i++){
+            if(fq[i]!=0 && fq[i]!=a){
                 return false;
             }
         }
