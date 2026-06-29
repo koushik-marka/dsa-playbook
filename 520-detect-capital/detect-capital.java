f97be3df2ca9f554a06c603fd2bf3342e7b31f1e
koushik <koushik.luffy007@gmail.com>
2026-06-29 19:50:48 +0530

    Time: 1 ms (89.10%) | Memory: 43.3 MB (47.21%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int c=0;
+        for(int i=0;i<word.length();i++){
+            if(Character.isUpperCase(word.charAt(i))){
+                c++;
+            }
+        }
+        if(c==1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+        else if(c==word.length()) return true;
+        else if(c==0) return true;
+        return false;
+        
+    }
+}
