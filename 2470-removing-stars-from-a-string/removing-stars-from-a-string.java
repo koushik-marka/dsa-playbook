6f319c2ff4bea1bf130638f9a45899c1cffe8bd7
koushik <koushik.luffy007@gmail.com>
2026-07-03 01:01:42 +0530

    Time: 28 ms (90.85%) | Memory: 48.1 MB (50.28%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch != '*'){
+                sb.append(ch);
+            }
+            else{
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
