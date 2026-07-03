cb96f9d336a7c74caa68f97304ab0b66b117c55f
koushik <koushik.luffy007@gmail.com>
2026-07-03 19:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (97.21%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -1,8 +1,11 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        return nums2[0]-nums1[0];
-        
+        int m1=nums1[0];
+        int m2=nums2[0];
+        for(int i=0;i<nums1.length;i++){
+            if(m1>nums1[i]) m1=nums1[i];
+            if(m2>nums2[i]) m2=nums2[i];
+        }
+        return m2-m1;
     }
 }
