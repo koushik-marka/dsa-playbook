b4cb45bd859516e8379644b27a9338d466c58cb4
koushik <koushik.luffy007@gmail.com>
2026-07-15 21:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.67%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        long l=1;
+        long r=num;
+        while(l<=r){
+            long m=l+(r-l)/2;
+            long sq=m*m;
+            if(sq==num){
+                return true;
+            }
+            else if(sq<num){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return false;
+    }
+}
