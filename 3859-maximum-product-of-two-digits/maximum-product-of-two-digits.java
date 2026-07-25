f958de1af222f812c2b63fc8abdb131d9c58dfd3
koushik <koushik.luffy007@gmail.com>
2026-07-26 00:20:17 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (85.43%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int n) {
+        int m=0;
+        int sm=0;
+        while(n>0){
+            int rem=n%10;
+            if(rem>m){
+                sm=m;
+                m=rem;
+            }
+            else if(rem>sm){
+                sm=rem;
+            }
+            n/=10;
+        }
+        return(m*sm);
+    }
+}
