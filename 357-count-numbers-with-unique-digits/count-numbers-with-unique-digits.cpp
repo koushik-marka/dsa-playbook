c08a5de73101614ed74eb9e06d242288bb76a408
koushik <koushik.luffy007@gmail.com>
2026-07-26 00:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.14%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n==0) return 1;
+        int r=10;
+        int a=9;
+        int b=9;
+        while(n>1){
+            a=a*b;
+            r+=a;
+            b--;
+            n--;
+        }
+        return r;
+    }
+};
