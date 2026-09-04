0790c1febb958055394661f8dae0ed168cfcfb27
koushik <koushik.luffy007@gmail.com>
2026-09-04 10:13:00 +0530

    Time: 13 ms (99.86%) | Memory: 42.2 MB (36.10%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -1,18 +1,18 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+   boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int s=0,e=n-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(isBadVersion(mid)){
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
+        int l = 1;
+        int r = n;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)) {
+                r = m - 1;
+            } else {
+                l = m + 1;
             }
         }
-        return s;
+        return l;
     }
 }
