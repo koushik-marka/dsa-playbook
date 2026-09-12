6faeee720b74899cc4683f00d605f7614988aa75
koushik <koushik.luffy007@gmail.com>
2026-09-12 12:12:08 +0530

    Time: 44 ms (98.80%) | Memory: 146.1 MB (9.77%) - LeetSync

--- 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.java
+++ 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.java
@@ -4,15 +4,16 @@ class Solution {
         int l2=discounts.length;
         Arrays.sort(prices);
         Arrays.sort(discounts);
-        int i=l1-1,j=l2-1;
+        int i=l1-1;
+        int j=l2-1;
         double s=0;
         while(i>=0 && j>=0){
-            s=s+ prices[i]*(100.0-discounts[j])/100.0;
+            s+=(prices[i]*(100.0-discounts[j])/100.0);
             i--;
             j--;
         }
         while(i>=0){
-            s=s+prices[i];
+            s+=prices[i];
             i--;
         }
         return s;
