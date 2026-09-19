fe636310e0bd94268fafa7c6c7585fcdffc2a73f
koushik <koushik.luffy007@gmail.com>
2026-09-19 11:10:55 +0530

    Time: 6 ms (3.35%) | Memory: 42.9 MB (99.35%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,28 +1,28 @@
 class Solution {
     public int calPoints(String[] op) {
         Stack<Integer> st=new Stack<>();
-    for(int i=0;i<op.length;i++){
-        String ch=op[i];
-        if(ch.equals("C")){
-            st.pop();
+        for(int i=0;i<op.length;i++){
+            String ch=op[i];
+            if(ch.equals("C")){
+                st.pop();
+            }
+            else if(ch.equals("D")){
+                st.push(st.peek()*2);
+            }
+            else if(ch.equals("+")){
+                int t=st.pop();
+                int t2=st.peek();
+                st.push(t);
+                st.push(t2+t);
+            }
+            else{
+                st.push(Integer.parseInt(ch));
+            }
         }
-        else if(ch.equals("D")){
-            st.push(st.peek()*2);
+        int av=0;
+        while(!st.isEmpty()){
+            av+=st.pop();
         }
-        else if(ch.equals("+")){
-            int t=st.pop();
-            int t2=st.peek();
-            st.push(t);
-            st.push(t2+t);
-        }
-        else{
-            st.push(Integer.parseInt(ch));
-        }
-    }
-    int av=0;
-    while(!st.isEmpty()){
-        av+=st.pop();
-    }
-    return av;
+        return av;
     }
 }
